10ca397ec2c0b6389cdc23992ce79ec7cf304846
DESKTOP-OFH8I45\Qiu <chengqiu03@gmail.com>
2019-05-31 00:14:22 +1000

    init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require ('express')
+const routes = require('./routes')
+
+const app = express()
+
+app.use(express.json())
+const seekers =[{'name':"zzz", "address":"test"}]
+const taskers =[{'name':"yyy", "address":"test1"}]
+const task =[{'catgory':"cleaning", "due":"01/06/19"}]
+
+
+// app.use('/v1', routes);
+
+app.get('/seeker',(req,res)=>{
+    res.json(seekers)
+})
+
+app.get('/tasker',(req,res)=>{
+  res.json(taskers)
+})
+
+app.get('/task',(req,res)=>{
+  res.json(task)
+})
+
+app.listen(8888,()=>{
+    console.log('server listening to port 8888')
+})
---
+++ src/routes/seeker.js
@@ -0,0 +1,22 @@
+const express = require('express');
+// const {
+//   getAllPost,
+//   getPostById,
+//   updatePostById,
+//   addPost,
+//   deletePostById
+// } = require('../controllers/posts');
+
+const router = express.Router();
+
+router.get('',);
+
+// router.get('/:id', validateId, getPostById);
+
+// router.post('', addPost);
+
+// router.put('/:id', validateId, updatePostById);
+
+// router.delete('/:id', validateId, deletePostById);
+
+module.exports = router;
