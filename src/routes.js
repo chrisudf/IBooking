fb8f51fbd4e9c3c9a89883f8da430f6d87f54f38
Cheng Qiu <chengqiu03@gmail.com>
2019-07-09 09:54:48 +1000

    add authGuard haven't implement for user validation

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,14 @@ const seekerRoute = require('./routes/seeker');
 const taskerRoute = require('./routes/tasker');
 const taskRoute = require('./routes/task');
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+
 const router = express.Router();
 
 router.use('/seeker', seekerRoute);
 router.use('/tasker', taskerRoute);
 router.use('/task', taskRoute);
 router.use('/users', userRoute);
+router.use('/auth', authRoute);
 
 module.exports = router;
