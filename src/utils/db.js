a1217ba511b53045d9d52d1a5c1f510d91054a9a
Cheng Qiu <chengqiu03@gmail.com>
2019-07-16 12:31:58 +1000

    ready to deploy

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
 
 exports.connectToDB =() =>{
-    const {DB_HOST,DB_PORT,DB_DATABASE} =process.env
-    const connectionString =`mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
-
-    mongoose.set('useFindAndModify',false)
-    return mongoose.connect(connectionString,{
-        useNewUrlParser:true,
-        useCreateIndex:true
+    const {DB_HOST,DB_PORT,DB_DATABASE,DB_PASSWORD, DB_USER } =process.env
+    let connectionString;
+    if (DB_PASSWORD && DB_USER) {
+      connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}?retryWrites=true`;
+    } else {
+      connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+    }
+    console.log(`Connecting to ${connectionString}`);
+    mongoose.set('useFindAndModify', false);
+    return mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useCreateIndex: true
     })
 }
